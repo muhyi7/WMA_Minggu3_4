e09fe0068e2636b89b3c48d84c599853a9f68507
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-09-05 07:33:15 +0700

    finish intent

--- app/src/main/java/com/polije/tif/wma_minggu3_4/MainActivity.java
+++ app/src/main/java/com/polije/tif/wma_minggu3_4/MainActivity.java
@@ -2,7 +2,9 @@ package com.polije.tif.wma_minggu3_4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scrollView(View view) {
+        Intent pindahScrollView = new Intent(MainActivity.this,ScrollView.class);
+        startActivity(pindahScrollView);
+    }
+
+    public void scrollViewHorizontal(View view) {
+        Intent pindahScrollViewHorizontal = new Intent(MainActivity.this,ScrollViewHorizontal.class);
+        startActivity(pindahScrollViewHorizontal);
     }
 }
